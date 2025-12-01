acb0110000860952be36efdcd9a1538b0c6099a0
VTSurmutyan <vtsurmutya@inno.tech>
2025-12-01 21:51:38 +0300

    Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
-package src;
-
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Длина текста: " + ".,m.,m.,m.,m,m.,m.,m.,m.,m,m.,m".length());
-        scanner.close();
+        int validFileCount = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            // Проверка на есть или нет файла
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Файл не существует.");
+                } else {
+                    System.out.println("Указанный путь является путём к папке, а не к файлу.");
+                }
+                continue;
+            }
+
+           
+            System.out.println("Путь указан верно.");
+            validFileCount++;
+            System.out.println("Это файл номер " + validFileCount);
+        }
     }
 }
